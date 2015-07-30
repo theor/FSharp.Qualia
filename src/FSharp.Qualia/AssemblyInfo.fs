aa3a4cd528390d3047cac20531d296dd173d8075
theor <theo.richart@gmail.com>
2015-07-30 09:02:41 -0400

    preparing 0.1-alpha.1 release

--- src/FSharp.Qualia/AssemblyInfo.fs
+++ src/FSharp.Qualia/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Qualia")>]
 [<assembly: AssemblyProductAttribute("FSharp.Qualia")>]
 [<assembly: AssemblyDescriptionAttribute("Event stream/rx based UI Framework and architecture")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "0.0.1"
