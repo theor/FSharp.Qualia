f45d8343fcece45d2e60796b41db38e6365da2be
theor <theo.richart@gmail.com>
2015-08-04 08:44:49 -0400

    continuing wpf collection tutorial

--- docs/content/tutorial.wpf.fsx
+++ docs/content/tutorial.wpf.fsx
@@ -10,6 +10,8 @@
 #r "PresentationCore.dll"
 #r "PresentationFramework.dll"
 #r "System.Numerics.dll"
+#r "System.Xaml.dll"
+#r "UIAutomationTypes.dll"
 #r "Rx-Core/lib/net45/System.Reactive.Core.dll"
 #r "Rx-Interfaces/lib/net45/System.Reactive.Interfaces.dll"
 #r "Rx-Linq/lib/net45/System.Reactive.Linq.dll"
@@ -20,6 +22,7 @@
 open System.Windows
 open FSharp.Qualia
 open System.Collections.ObjectModel
+open System.Windows.Controls
 
 (**
 Collections
@@ -28,18 +31,34 @@ Collections
 
 type Events = Add
 
+
+type ListViewWindow() as x =
+    inherit Window()
+    let label = Label()
+    let button = Button(Content="Add")
+    let list = ListBox()
+    do
+        let sp = StackPanel()
+        sp.Children.Add label |> ignore
+        sp.Children.Add list |> ignore
+        sp.Children.Add button |> ignore
+        x.Content <- sp
+    member val Label = label
+    member val Button = button
+    member val List = list
+
 type ItemModel() =
     member val Text = ReactiveProperty("")
 
 type ItemView(elt, m) =
-    inherit View<Events, FrameworkElement, ItemModel>(elt, m)
+    inherit View<Events, Label, ItemModel>(elt, m)
      override x.EventStreams = []
-     override x.SetBindings m = ()
+     override x.SetBindings m = elt.Content <- m.Text.Value
 type ListModel() =
     member val Items = new ObservableCollection<ItemModel>()
     
 type ListView(elt, m) =
-    inherit View<Events, Window, ListModel>(elt, m)
+    inherit View<Events, ListViewWindow, ListModel>(elt, m)
     override x.EventStreams = []
     override x.SetBindings m = ()
 type ListController() =
@@ -49,10 +68,10 @@ type ListController() =
             function
             | Add -> Sync (fun m -> ())
 
-let w = new Window()
+let w = new ListViewWindow()
 let app = if Application.Current = null then Application() else Application.Current
 let lm = ListModel()
-let v = ListView(Window(),lm)
+let v = ListView(w,lm)
 let c = ListController()
 let loop = EventLoop(v, c)
 loop.Start()
