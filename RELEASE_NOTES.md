d9b0e4567d78755e38fd6edc2864b3d35052002a
Theo Richart <theo.richart@gmail.com>
2015-08-14 00:30:15 -0400

    Bump version to 0.1.3

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.1.3 - August 14 2015
+* view composition with a different event type, aggregated in the main view event type
+
 #### 0.1.2 - August 05 2015
 * bugfix: DerivedCollection events
 
--- src/FSharp.Qualia/AssemblyInfo.fs
+++ src/FSharp.Qualia/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Qualia")>]
 [<assembly: AssemblyProductAttribute("FSharp.Qualia")>]
 [<assembly: AssemblyDescriptionAttribute("Event stream/rx based UI Framework and architecture")>]
-[<assembly: AssemblyVersionAttribute("0.1.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.2")>]
+[<assembly: AssemblyVersionAttribute("0.1.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.2"
+    let [<Literal>] Version = "0.1.3"
